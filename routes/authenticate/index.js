bda6e92319a309858028e440d2b35065bd63972b
Sadegh Hayeri <sadegh.hayeri@tap30.com>
2020-05-15 02:44:32 +0430

    Add user api

--- routes/authenticate/index.js
+++ routes/authenticate/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 const authenticateApp = require('../../application/authenticate');
 const HttpStatus = require('http-status-codes');
+const { check, validationResult } = require('express-validator');
 
-router.post('/', async (req, res) => {
+router.post('/', [
+    check('usernameOrEmail').exists(),
+    check('password').exists(),
+], async (req, res) => {
     const {usernameOrEmail, password} = req.body;
     const {user, token} = await authenticateApp.authenticate(usernameOrEmail, password);
     res.status(HttpStatus.OK).json({user, token});
---
+++ routes/users/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const userApp = require('../../application/users');
+const HttpStatus = require('http-status-codes');
+const authorize = require('../../middlewares/authorize');
+const {ROLES} = require('../../config/enums');
+const codeStrings = require('../../config/codeStrings');
+const { check, validationResult } = require('express-validator');
+
+router.get('/', authorize(ROLES.ADMIN), async (req, res) => {
+    const users = await userApp.getUsers();
+    res.status(HttpStatus.OK).json({users});
+});
+
+router.get('/:id', authorize(), async (req, res) => {
+    const requestedId = parseInt(req.params.id);
+    if (req.user.role === ROLES.ADMIN || req.user.id === requestedId) {
+        const user = await userApp.getUser(req.user, requestedId);
+        res.status(HttpStatus.OK).json({user});
+    } else {
+        res.status(HttpStatus.FORBIDDEN).json({codeString: codeStrings.FORBIDDEN_ERROR});
+    }
+});
+
+router.post('/', [
+    check('email').isEmail(),
+    check('username').isAlphanumeric(),
+    check('password').isLength({ min: 5 }),
+], async (req, res) => {
+    const {email, username, password} = req.body;
+    const user = await userApp.newUser(email, username, password);
+    res.status(HttpStatus.OK).json({user});
+});
+
+module.exports = router;
