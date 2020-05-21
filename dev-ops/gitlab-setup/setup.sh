47f73617c06dcec7cfbdf271fcbdc6dce6ce7175
Sadegh Hayeri <sadegh.hayeri@tap30.com>
2020-05-21 18:05:18 +0430

    Add rbac permission

--- dev-ops/gitlab-setup/setup.sh
+++ dev-ops/gitlab-setup/setup.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+helm uninstall k8-docker-alpha
+helm uninstall k8-docker-bravo
+helm uninstall k8-docker-charlie
+
 helm install k8-docker-alpha gitlab/gitlab-runner -f runner-config.yaml
 helm install k8-docker-bravo gitlab/gitlab-runner -f runner-config.yaml
 helm install k8-docker-charlie gitlab/gitlab-runner -f runner-config.yaml
