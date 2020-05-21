8dfbcdc858e7d766ec2863e7aa299c2388abda38
Sadegh Hayeri <sadegh.hayeri@tap30.com>
2020-05-22 03:21:23 +0430

    Add deployer

--- dev-ops/gitlab-setup/setup.sh
+++ dev-ops/gitlab-setup/setup.sh
@@ -3,7 +3,9 @@
 helm uninstall k8-docker-alpha
 helm uninstall k8-docker-bravo
 helm uninstall k8-docker-charlie
+helm uninstall k8-deployer-delta
 
-helm install k8-docker-alpha gitlab/gitlab-runner -f runner-config.yaml
-helm install k8-docker-bravo gitlab/gitlab-runner -f runner-config.yaml
-helm install k8-docker-charlie gitlab/gitlab-runner -f runner-config.yaml
+helm install k8-docker-alpha gitlab/gitlab-runner -f k8-docker-config.yaml
+helm install k8-docker-bravo gitlab/gitlab-runner -f k8-docker-config.yaml
+helm install k8-docker-charlie gitlab/gitlab-runner -f k8-docker-config.yaml
+helm install k8-deployer-alpha gitlab/gitlab-runner -f k8-deployer-config.yaml
