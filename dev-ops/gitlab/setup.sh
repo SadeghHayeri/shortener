32581e063c454f33316398683f4c1499b8058229
Sadegh Hayeri <sadegh.hayeri@tap30.com>
2020-05-22 05:15:54 +0430

    Rename to kubectl

--- dev-ops/gitlab/setup.sh
+++ dev-ops/gitlab/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 helm uninstall k8-docker-genernator
-helm uninstall k8-deployer-genernator
+helm uninstall k8-kubectl-genernator
 
 helm install k8-docker-genernator gitlab/gitlab-runner -f k8-docker-config.yaml
-helm install k8-deployer-genernator gitlab/gitlab-runner -f k8-deployer-config.yaml
+helm install k8-kubectl-genernator gitlab/gitlab-runner -f k8-kubectl-config.yaml
